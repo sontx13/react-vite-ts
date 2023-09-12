4fe9397f36a0b543a08d49303bb29f0811366450
sontx13 <sontx13@gmail.com>
2023-09-12 22:06:27 +0700

    antd

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -1,44 +1,77 @@
+import { useEffect, useState } from 'react';
 import '../../style/user.css'
+
+interface IUser{
+    _id:string,
+    email:string,
+    name:string,
+    role:string
+}
 const UserTable = () =>{
+   
+    const [listUser,setListUser]= useState([]);
+    useEffect(()=>{
+        //console.log(">>> check useEffect");
+        getData()
+    },[])
+
+    const getData = async() =>{
+        const res = await fetch("http://localhost:8000/api/v1/auth/login",{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username: "sontx13@gmail.com",
+                    password: "123456"
+                })
+        });
+        const data = await res.json();
+        //console.log(">>> data==" +  JSON.stringify(data));
+
+        const access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjRlZDljNmMxZjM5MzkxNWNlMjVmNGNkIiwibmFtZSI6IlRy4bqnbiBYdcOibiBTxqFuIiwiZW1haWwiOiJzb250eDEzQGdtYWlsLmNvbSIsInJvbGUiOnsiX2lkIjoiNjRlZDljNmMxZjM5MzkxNWNlMjVmNGM3IiwibmFtZSI6IlNVUEVSX0FETUlOIn0sImlhdCI6MTY5NDUyMzE3MSwiZXhwIjoxNjk0NjA5NTcxfQ.wxdhmeyH4XfDuets-0ymR-ZVFTbbdR1atDpe0QR3hIQ";
+
+        const res_list_user = await fetch("http://localhost:8000/api/v1/users?current=1&pageSize=5",{
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${access_token}`
+                },
+        });
+        const d = await res_list_user.json();
+
+        //console.log(">>> listUser==" +  JSON.stringify(d.data.result));
+
+        setListUser(d.data.result);
+        
+    }
+
+    console.log(">>> check re-reder"+JSON.stringify(listUser));
     return(
         <div>
-            <h2>HTML Table</h2>
+            <h2>Table User</h2>
             <table>
-            <tr>
-                <th>Company</th>
-                <th>Contact</th>
-                <th>Country</th>
-            </tr>
-            <tr>
-                <td>Alfreds Futterkiste</td>
-                <td>Maria Anders</td>
-                <td>Germany</td>
-            </tr>
-            <tr>
-                <td>Centro comercial Moctezuma</td>
-                <td>Francisco Chang</td>
-                <td>Mexico</td>
-            </tr>
-            <tr>
-                <td>Ernst Handel</td>
-                <td>Roland Mendel</td>
-                <td>Austria</td>
-            </tr>
-            <tr>
-                <td>Island Trading</td>
-                <td>Helen Bennett</td>
-                <td>UK</td>
-            </tr>
-            <tr>
-                <td>Laughing Bacchus Winecellars</td>
-                <td>Yoshi Tannamuri</td>
-                <td>Canada</td>
-            </tr>
-            <tr>
-                <td>Magazzini Alimentari Riuniti</td>
-                <td>Giovanni Rovelli</td>
-                <td>Italy</td>
-            </tr>
+                <thead>
+                    <tr>
+                        <td>Email</td>
+                        <td>Name</td>
+                        <td>Role</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        listUser.map((item:IUser)=> {
+                            return(
+                                <tr key={item._id}>
+                                    <td>{item.email}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.role}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                 
+                </tbody>
             </table>
         </div>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,74 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {  useState } from 'react';
+import App from './App.tsx';
 import {
   createBrowserRouter,
+  Link,
+  Outlet,
   RouterProvider,
 } from "react-router-dom";
-import './index.css'
+import './index.css';
+import { AppstoreOutlined, HomeOutlined, UserOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Menu } from 'antd';
 import UserPage from './screens/users.page.tsx';
 
+const items: MenuProps['items'] = [
+  {
+    label: <Link to='/'>Home</Link>,
+    key: 'home',
+    icon: <HomeOutlined />,
+  },
+  {
+    label: <Link to='/users'>Users</Link>,
+    key: 'users',
+    icon: <UserOutlined />,
+  },
+
+];
+
+const Header: React.FC = () => {
+  const [current, setCurrent] = useState('home');
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
+  return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+};
+
+
+const LayoutAdmin = () => {
+  return (
+    <div>
+      <Header/>
+      <Outlet/>
+      <footer>footer</footer>
+    </div>
+  )
+}
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <LayoutAdmin />,
+    children:[
+      {
+        index:true,
+        element: <App/>,
+      },
+      {
+        path: "users",
+        element: <UserPage/>,
+      },
+      {
+        path: "tracks",
+        element: <div>manage tracks</div>,
+      },
+    ]
   }, 
-  {
-    path: "/users",
-    element: <UserPage/>,
-  },
-  {
-    path: "/tracks",
-    element: <div>manage tracks</div>,
-  },
+ 
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
