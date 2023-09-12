4ea5b4ec4224ff0d8035e7e5182f62527fff78ea
sontx13 <sontx13@gmail.com>
2023-09-12 19:25:55 +0700

    user page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-//import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import UserPage from './screens/users.page.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  }, 
+  {
+    path: "/users",
+    element: <UserPage/>,
+  },
+  {
+    path: "/tracks",
+    element: <div>manage tracks</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
